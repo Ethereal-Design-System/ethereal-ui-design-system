d96ecf98de5c7cbb44904b4bbbc8799d36732eff
Thalya Coimbra <87552041+thalya-coimbra@users.noreply.github.com>
2026-07-28 20:21:23 -0300

    Create Field components (#8)

---
+++ src/components/Field/variants/messageVariants.ts
@@ -0,0 +1,10 @@
+import { tokens } from '@/tokens';
+
+const { colors } = tokens;
+
+export const messageVariants = {
+  default: { color: colors.brand.secondary[900] },
+  error: {
+    color: colors.feedback.error.dark,
+  },
+};
