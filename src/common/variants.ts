41e8613078ca9cb8292a91afb7241ea78e7c067f
Thalya Steffany <contato.thalyasteffany@gmail.com>
2026-07-28 21:20:49 -0300

    refactor(components): create reusable 'IS_WEB_PLATFORM' constant

--- src/common/variants.ts
+++ src/common/variants.ts
@@ -1,10 +1,8 @@
 import { tokens } from '@/tokens';
-import { Platform } from 'react-native';
+import { IS_WEB_PLATFORM } from './constants';
 
 const { fontSize, lineHeight } = tokens;
 
-const isWebPlatform = Platform.OS === 'web';
-
-export const formFontSizeVariants = isWebPlatform
+export const formFontSizeVariants = IS_WEB_PLATFORM
   ? { fontSize: fontSize.base, lineHeight: lineHeight.base }
   : { fontSize: fontSize.sm, lineHeight: lineHeight.sm };
--- src/components/Field/styles.tsx
+++ src/components/Field/styles.tsx
@@ -1,10 +1,9 @@
+import { IS_WEB_PLATFORM } from '@/common/constants';
 import { formFontSizeVariants } from '@/common/variants';
 import { tokens } from '@/tokens';
-import { Platform, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const { spacing, fontFamily, colors, fontSize } = tokens;
-
-const isWebPlatform = Platform.OS === 'web';
 
 export const fieldStyles = StyleSheet.create({
   root: {
@@ -20,7 +19,7 @@ export const fieldStyles = StyleSheet.create({
     fontFamily: fontFamily.body[500],
   },
   message: {
-    fontFamily: fontFamily.body[isWebPlatform ? 400 : 500],
-    fontSize: isWebPlatform ? fontSize.xs : fontSize.base,
+    fontFamily: fontFamily.body[IS_WEB_PLATFORM ? 400 : 500],
+    fontSize: IS_WEB_PLATFORM ? fontSize.xs : fontSize.base,
   },
 });
